ef951bb83d71f708e69c9f50ec30f34e6b42b6ea
_-=Miyagi=-_ <ppamuzhakk@mail.ru>
2024-02-13 18:23:04 +0300

    Домашнее задание 09.02.24

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import Homework0902.Run0902;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -6,21 +8,7 @@ import java.util.stream.IntStream;
 
 public class Main {
     public static void main(String[] args) {
-        String myName = "Памужак Петр Дмитриевич";
-
-        int[] intArr = new int[]{2, 45, 13, -42, 63, 12, 43, 53, 12, 0, -4};
-        List<Integer> intList = IntStream.rangeClosed(1, 10).boxed().collect(ArrayList::new, ArrayList::add, ArrayList::addAll);
-
-        System.out.println("Работа с array:");
-        System.out.println("Исходный массив: " + Arrays.toString(intArr));
-        System.out.println("Сумма: " + Arrays.stream(intArr).sum());
-        System.out.println("Среднее: " + Arrays.stream(intArr).average().orElse(0));
-        System.out.println("\nРабота с List:");
-        System.out.println("Исходный лист: " + intList);
-        System.out.println("Сумма: " + intList.stream().mapToInt(Integer::intValue).sum());
-        System.out.println("Среднее: " + intList.stream().mapToInt(Integer::intValue).average().orElse(0));
-        System.out.println();
-
-        System.out.println(myName);
+        Run0902 run0902 = new Run0902();
+        run0902.run();
     }
 }
